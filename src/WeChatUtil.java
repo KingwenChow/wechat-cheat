a680d4028f37ed04ebbf5e45d0e7e714be79c607
zhang zhong nan <B13040521@njupt.edu.cn>
2014-04-27 19:11:12 +0800

    用java写的控制器

---
+++ src/WeChatUtil.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author B5B6CACE
+ */
+public class WeChatUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+    
+}
